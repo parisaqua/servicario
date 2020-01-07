7470005ea26a512ce581c200109231fd6c24bbac
Fred <frederic@pmo.digital>
2020-01-07 22:05:19 +0100

    Etape 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h1>I am Home Page</h1>
-            <h2>ku vtuykv kuyv uk v</h2>
-        </div>
-    )
+// const Home = () => {
+//     return (
+//         <div className="container">
+//             <h1>I am Home Page</h1>
+//             <h2>ku vtuykv kuyv uk v</h2>
+//         </div>
+//     )
+// }
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <h1>I am Home Class Page</h1>
+                <h2>ku vtuykv kuyv uk v</h2>
+            </div>
+        )
+    }
 }
 
 export default Home;
+
