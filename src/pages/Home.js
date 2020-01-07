567fb8a18b5b706b32e12b45f4fbc13be0486014
Fred <frederic@pmo.digital>
2020-01-07 21:47:36 +0100

    Etape 4

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>I am Home Page</h1>
+            <h2>ku vtuykv kuyv uk v</h2>
+        </div>
+    )
+}
+
+export default Home;
