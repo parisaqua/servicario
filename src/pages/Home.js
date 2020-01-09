963b990a94d7d4e60ec64025b631261b5caae3d2
Frédéric BASTIAN <frederic@bastian.paris>
2020-01-09 09:06:58 +0100

    Etape 12

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,46 @@ import React, { useState, useEffect } from 'react';
 import Counter from "../components/Counter";
 import './Home.css';
 
+class Home extends React.Component {  
+    
+    state = {
+        message: 'Super message !!!',
+        testNumber: 99
+    }
+
+    componentDidMount () {
+        setTimeout(() => {
+            this.setState({message: 'I have updated the message!'})
+        }, 1000) 
+    }
+
+    displayMessage = (type, count) => {
+        if(type === 'increment') {
+             //alert(`Your number was incremented! Current value: ${count}`)
+             return
+        }
+        //alert(`Your number was decremented! Current value: ${count}`)
+    }
+
+    render() {
+        const {message, testNumber} = this.state;
+        return (
+            <div className='container'>
+                <h1>I am home page.</h1>
+                <p>{message}</p>
+                <button onClick={() => this.setState({testNumber: testNumber + 1})}>Testing Increment</button>
+                <Counter 
+                    testNumber={testNumber}
+                    onChange={this.displayMessage}
+                    title={'I am counter component'} 
+                />
+            </div>
+        )
+    }
+}
+
+export default Home;
+
 // const Home = () => {
     
 //     const [message, setMessage] = useState('Super message à moi !')
@@ -23,53 +63,12 @@ import './Home.css';
 //     }
 
 //     return (
-        // <div className='container'>
-        //     <h1>I am home page.</h1>
-        //     <p>{message}</p>
-        //     <button onClick={increment}>Incrément</button>
-        //     <div className="counter">{count}</div>
-        //     <button onClick={decrement}>Decrement</button>
-        // </div>
+//         <div className='container'>
+//             <h1>I am home page.</h1>
+//             <p>{message}</p>
+//             <button onClick={increment}>Incrément</button>
+//             <div className="counter">{count}</div>
+//             <button onClick={decrement}>Decrement</button>
+//         </div>
 //     )
 // }
-
-
-class Home extends React.Component {  
-    
-    state = {
-        message: 'Super message !!!'
-    }
-
-    componentDidMount () {
-        setTimeout(() => {
-            this.setState({message: 'I have updated the message!'})
-        }, 1000) 
-    }
-
-    displayMessage = (type, count) => {
-        if(type === 'increment') {
-             alert(`Your number was incremented! Current value: ${count}`)
-             return
-        }
-
-        alert(`Your number was decremented! Current value: ${count}`)
-       
-    }
-
-    render() {
-        const {message} = this.state;
-        return (
-            <div className='container'>
-                <h1>I am home page.</h1>
-                <p>{message}</p>
-                <Counter 
-                    onChange={this.displayMessage}
-                    title={'I am counter component'} 
-                />
-            </div>
-        )
-    }
-}
-
-export default Home;
-
